3b7a86f59eeba7bc785f053be0826e4186cb87d1
rokzz13 <ritikvijayvargiya16@gmail.com>
2020-12-10 14:45:25 +0530

    interface

---
+++ account.js
@@ -0,0 +1,15 @@
+var fname = document.getElementById("fname");
+var lname = document.getElementById("lname");
+var email = document.getElementById("email");
+var address = document.getElementById("addr");
+var mobile = document.getElementById("mobn");
+var date = document.getElementById("birthday");
+var pass = document.getElementById("pass");
+var cpass = document.getElementById("cpass");
+var check = document.getElementById("check");
+var btn= document.getElementById("acc");
+
+if (fname="")
+{
+    document.write("pease enter username");
+}
---
+++ login.js
@@ -0,0 +1,14 @@
+function validation()
+{
+    var user = document.getElementById("uname").value;
+    
+    var pass = document.getElementById("pws").value;
+    
+    var user = document.getElementById("uname").value;
+
+    if(user=="")
+    {
+   alert("tjngodfngf");
+    }
+
+}
